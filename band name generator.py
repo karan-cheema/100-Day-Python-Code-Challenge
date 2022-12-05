7f0569b5e8441519809ff723e73fed188d8a52c2
Karan Cheema <24313872+karan-cheema@users.noreply.github.com>
2022-12-05 12:09:34 +0530

    Add files via upload

---
+++ band name generator.py
@@ -0,0 +1,4 @@
+print("Welcome to band name generator.\nAnswer few questions and we will generate a band name for you!")
+city = str(input("What is the name of the city you grew up in? "))
+nickname = str(input("What is your nickname? "))
+print(f"Your band name is {city} {nickname}")
